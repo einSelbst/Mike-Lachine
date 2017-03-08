de4073f308d52e39674290434e907eb9685c51e0
Silvio <silvio@sintuity.com>
2017-03-08 17:55:37 +0100

    ADD query to order posts by day and number of likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,7 @@ class Post < ApplicationRecord
   validates :title, presence: true # max length?
   validates :content, presence: true # max length?
 
+  def self.order_by_day_and_likes
+    Post.order("date_trunc('day', created_at) DESC, likes_count DESC")
+  end
 end
