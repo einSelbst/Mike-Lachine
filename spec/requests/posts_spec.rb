3f77b033f601903039a9164a715d6573e22dd64a
Silvio <silvio@sintuity.com>
2017-03-08 15:16:50 +0100

    IMP make adding new post work

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe "Posts", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /posts" do
+    let(:valid_post) { attributes_for(:post) }
+
+    it "responds with Created (201)" do
+      skip "need to set a current user"
+      post posts_path, params: { post: valid_post }
+      expect(response).to have_http_status(:created)
+    end
+  end
 end
