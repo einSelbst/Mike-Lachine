750707a8c2bf97567f05eaf7c023f69b51123e4e
Silvio <silvio@sintuity.com>
2017-03-08 17:37:32 +0100

    ADD counter cache for likes of post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308141800) do
+ActiveRecord::Schema.define(version: 20170308163508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170308141800) do
     t.text "content", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
